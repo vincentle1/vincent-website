3ae62052fdfed195eac61b090b350a5a93a61f00
vincentle1 <levincent19@gmail.com>
2020-04-18 20:12:34 -0700

    Navbar now lines up with close

--- scripts/script-sheet.js
+++ scripts/script-sheet.js
@@ -1,60 +1,24 @@
 
 function toggleMenu(){
-
-    const links = document.querySelector("nav ul.links");
-    const topBar = document.querySelector(".topBar");
-    const menuButton = document.querySelector('li.toggle');
-    const name = const name = document.querySelector('li.name');
-    links.classList.toggle("openLinks");
-    topBar.classList.toggle("expandTopBar");
-
-    menuButton.addEventListener('click', toggleMenu); 
-
     if (menuButton.id == "closed"){
     /* 1. Toggle between Hamburger and Close */
         menuButton.setAttribute("id", "open");
+        menuButton.setAttribute("style", "margin-right:0")
         hamburger.style.display = "none";
         closeBar.setAttribute("style", 
-        "display: block; font-size: 24px, padding-right: 20px");
+        "display: block; font-size: 20px; padding-right: 0px; margin-right: 10px");
     /* 2. Make name appear and disappear */
         name.style.visibility = "hidden";
     /* 3. Make background color gray for top bar */
         topBar.style.backgroundColor = "#E1E1E1";
         menuButton.style.border = "none";
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function toggleMenu(){
-    if (menuButton.id == "closed"){
-    /* 1. Toggle between Hamburger and Close 
-        menuButton.setAttribute("id", "open");
-        hamburger.style.display = "none";
-        closeBar.setAttribute("style", 
-        "display: block; font-size: 24px, padding-right: 20px");
-    /* 2. Make name appear and disappear 
-        name.style.visibility = "hidden";
-    /* 3. Make background color gray for top bar 
-        topBar.style.backgroundColor = "#E1E1E1";
-        menuButton.style.border = "none";
-    /* 4. Make all links appear
+    /* 4. Make all links appear */
         links.style.display = "block";
         
 }
     else if (menuButton.id == "open"){
         menuButton.setAttribute("id", "closed");
+        menuButton.setAttribute("style", "margin-right:20")
         closeBar.style.display = "none";
         hamburger.style.display = "block";
         name.style.visibility = "visible";
@@ -64,13 +28,12 @@ function toggleMenu(){
     }
 }
 
-
 const menuButton = document.querySelector('li.toggle');
-const closeBar = document.querySelector('li.toggle span');
+const closeBar = document.querySelector('span');
 const name = document.querySelector('li.name');
 const hamburger = document.querySelector('i#hamburger');
 const topBar = document.querySelector(".topBar");
 const links = document.querySelector("nav ul.links");
 
 menuButton.addEventListener('click', toggleMenu);
-*/
+
