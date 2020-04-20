cd72a3fb4a85745d21b9e050b165578913097925
vincentle1 <levincent19@gmail.com>
2020-04-19 21:36:50 -0700

    Bar can collapse and overlap, need to check if it can scroll

--- scripts/script-sheet.js
+++ scripts/script-sheet.js
@@ -1,79 +1,29 @@
+const navLinks = document.querySelector(".headerLinks");
+const toggleButton = document.querySelector(".toggleButton");
+const burger = document.querySelector("#burger");
+const close = document.querySelector("span");
+const name = document.querySelector(".name");
+const topBar = document.querySelector(".nameAndButton");
 
 function toggleMenu(){
+    navLinks.classList.toggle("expand");
+    close.classList.toggle("expand");
+
+    burger.classList.toggle("disappear");
+    name.classList.toggle("disappear");
     
-    navMenu.classList.toggle("expandMenu");
-    menuButton.classList.toggle("close");
-    topBar.classList.toggle("changeTopBar");
-
+    topBar.classList.toggle("addColor");
+    topBar.classList.toggle("removePadding");
+    toggleButton.classList.toggle("grow");
 }
 
-const navMenu = document.querySelector(".navMenu");
-const menuButton = document.querySelector("li.switch");
-const topBar = document.querySelector(".topBar");
+toggleButton.addEventListener("click", toggleMenu);
 
-menuButton.addEventListener("click", toggleMenu); 
-
-    /* 1. Toggle between Hamburger and Close 
-        menuButton.setAttribute("id", "open");
-        hamburger.style.display = "none";
-        closeBar.setAttribute("style", 
-        "display: block; font-size: 24px, padding-right: 20px");
-    /* 2. Make name appear and disappear */
-       /* name.style.visibility = "hidden";
-    /* 3. Make background color gray for top bar 
-        topBar.style.backgroundColor = "#E1E1E1";
-        menuButton.style.border = "none";
-}
-*/
+/*After clicking on link, close the nav*/
+navLinks.addEventListener("click", toggleMenu);
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-/*function toggleMenu(){
-    if (menuButton.id == "closed"){
-    /* 1. Toggle between Hamburger and Close 
-        menuButton.setAttribute("id", "open");
-        hamburger.style.display = "none";
-        closeBar.setAttribute("style", 
-        "display: block; font-size: 24px, padding-right: 20px");
-    /* 2. Make name appear and disappear 
-        name.style.visibility = "hidden";
-    /* 3. Make background color gray for top bar 
-        topBar.style.backgroundColor = "#E1E1E1";
-        menuButton.style.border = "none";
-    /* 4. Make all links appear
-        links.style.display = "block";
-        
-}
-    else if (menuButton.id == "open"){
-        menuButton.setAttribute("id", "closed");
-        closeBar.style.display = "none";
-        hamburger.style.display = "block";
-        name.style.visibility = "visible";
-        topBar.style.backgroundColor = "white";
-        menuButton.style.border = "2px solid #d0d0d0";
-        links.style.display = "none";
-    }
-}
-
-
-const menuButton = document.querySelector('li.toggle');
-const closeBar = document.querySelector('li.toggle span');
-const name = document.querySelector('li.name');
-const hamburger = document.querySelector('i#hamburger');
-const topBar = document.querySelector(".topBar");
-const links = document.querySelector("nav ul.links");
-
-menuButton.addEventListener('click', toggleMenu);
-*/
