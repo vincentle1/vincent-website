a666d2feed45df74334f5b61c0385774b850d8d9
vincentle1 <levincent19@gmail.com>
2020-04-23 16:35:13 -0700

    added drop shadow that appears on scroll for navbar

--- scripts/script-sheet.js
+++ scripts/script-sheet.js
@@ -1,10 +1,10 @@
+
 const navLinks = document.querySelector(".links");
 const toggleButton = document.querySelector(".toggleButton");
 const burger = document.querySelector("#burger");
 const close = document.querySelector("span");
 const name = document.querySelector(".name");
 const topBar = document.querySelector(".nameAndButton");
-const content = document.querySelector(".allSections");
 const specificNavLinks = document.querySelectorAll(".links li a");
 
 function toggleMenu(){
@@ -25,6 +25,24 @@ toggleButton.addEventListener("click", toggleMenu);
 /*After clicking on link, close the nav*/
 specificNavLinks.forEach(function(navLink){
     navLink.addEventListener("click", toggleMenu)});
+
+/*Drop shadow on scroll*/
+
+//Offset-height returns height of element//
+const nav_height = topBar.offsetHeight;
+
+function dropShadow(){
+if (window.scrollY >= nav_height){
+    topBar.classList.add("addShadow");
+} else {
+    topBar.classList.remove("addShadow");
+}
+}
+
+window.addEventListener("scroll", dropShadow);
+
+
+
 
 
 
