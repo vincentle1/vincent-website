4c5ddf1c4bf3c52866e286c3b16344cc8a875204
vincentle1 <levincent19@gmail.com>
2020-04-27 17:56:35 -0700

    Disable scrolling when menu open, changed color of nav to gradient

--- scripts/script-sheet.js
+++ scripts/script-sheet.js
@@ -1,5 +1,6 @@
-
+const body = document.querySelector("body");
 const navLinks = document.querySelector(".links");
+const header = document.querySelector("header");
 const toggleButton = document.querySelector(".toggleButton");
 const burger = document.querySelector("#burger");
 const close = document.querySelector("span");
@@ -10,6 +11,7 @@ const specificNavLinks = document.querySelectorAll(".links li a");
 function toggleMenu(){
 
     navLinks.classList.toggle("expand");
+    header.classList.toggle("animate");
     close.classList.toggle("closeAppear");
 
     burger.classList.toggle("disappear");
@@ -17,8 +19,13 @@ function toggleMenu(){
 
     topBar.classList.toggle("removeBorder");
     toggleButton.classList.toggle("grow");
+
+    var head_height = header.offsetHeight;
+    body.classList.toggle("disableScroll");
+    topBar.classList.toggle("disableScroll");
     
 }
+
 
 toggleButton.addEventListener("click", toggleMenu);
 
@@ -27,12 +34,12 @@ specificNavLinks.forEach(function(navLink){
     navLink.addEventListener("click", toggleMenu)});
 
 /*Drop shadow on scroll*/
-
 //Offset-height returns height of element//
 const nav_height = topBar.offsetHeight;
 
+
 function dropShadow(){
-if (window.scrollY >= nav_height){
+if ((window.scrollY >= nav_height) && (navLinks.classList.length <=2)){
     topBar.classList.add("addShadow");
 } else {
     topBar.classList.remove("addShadow");
@@ -40,7 +47,6 @@ if (window.scrollY >= nav_height){
 }
 
 window.addEventListener("scroll", dropShadow);
-
 
 
 
